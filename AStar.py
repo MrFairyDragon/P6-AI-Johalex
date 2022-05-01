bdcb22129bfb8714fb621ea61b3790a4240230f6
MrFairyDragon <60883151+MrFairyDragon@users.noreply.github.com>
2022-05-01 10:24:33 +0200

    Fixed some wierd A star movements

--- AStar.py
+++ AStar.py
@@ -18,9 +18,9 @@ def calc_h_score(p1, p2):
 
 def get_cost(tile):
     if tile.type == TileList.MUD:
-        return 3
+        return 20
     if tile.type == TileList.SAND:
-        return 2
+        return 10
     else:
         return 1
 
@@ -40,16 +40,15 @@ class AStar:
         while tiles_needs_checking:
             current = queue.get()[2]
             tiles_needs_checking.remove(current)
-            print("test")
             if current == end:
                 return make_path(current, parent)
             for neighbour in current.neighbors:
                 print(neighbour)
-                temp_g_score = g_score[current] + 1
+                temp_g_score = g_score[current] + get_cost(neighbour)
                 if temp_g_score < g_score[neighbour]:
                     parent[neighbour] = current
                     g_score[neighbour] = temp_g_score
-                    f_score[neighbour] = temp_g_score + (calc_h_score(neighbour.worldPosition, end.worldPosition)*get_cost(neighbour))
+                    f_score[neighbour] = temp_g_score + (calc_h_score(neighbour.worldPosition, end.worldPosition) + get_cost(neighbour))
 
                     if neighbour not in tiles_needs_checking:
                         counter += 1
